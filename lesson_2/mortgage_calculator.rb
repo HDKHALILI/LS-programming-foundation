93edb85a549bd1f0e245bb6cd41c570fde4315c7
Hadi Khalili <14176079+HDKHALILI@users.noreply.github.com>
2019-07-29 13:53:14 +1000

    refactor mortgage_calculator.rb

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -1,40 +1,23 @@
-# I will need three pieces of information
-#   - the loan amount
-#   - the annual Percentage Rate (APR)
-#   - the loan duration
-
-# I need to calculate
-#   - monthly interest rate
-#   - loan duration in months
-
-#  Formula:
-# m = p * (j /(1- (1 + j)**(-n)))
-# m = mothly payment
-# p = loan amount
-# j = monthly interest rate
-# n = loan duration in moths
-
-# messages
 MESSAGE = {
   welcome: "Welcome to Loan Calculator",
   amount: "What is the loan amount?",
   rate: "What is the Anual Percentage Rage (APR)? (enter 5 for 5%)",
   duration: "What is the loan duration in years?",
-  invalid_number: "Please enter a valid positive number",
+  invalid_amount: "Please enter a correct loan amount e.g. 100000",
+  invalid_rate: "Please enter a correct rate (5 for 5% or 3.4 for 3.4%)",
+  invalid_term: "Please enter a valid term between 1 and 30 years",
+  calculate_again: "Would you like to calculate another mortgage? (y or n)",
   thank_you: "Thank you for using the Loan Calculator"
 }
 
 #  helper methods
-def valid_number?(input)
-  input.to_i != 0
+def valid_integer(input)
+  # should not start with 0
+  /^[1-9]\d*$/.match(input)
 end
 
 def float?(input)
-  /\d/.match(input) && /^\d*\.?\d*$/.match(input)
-end
-
-def number?(input)
-  valid_number?(input) || float?(input)
+  /\A[+-]?\d+(\.[\d]+)?\z/.match(input)
 end
 
 def prompt(message)
@@ -43,66 +26,83 @@ end
 
 def monthly_payment(amount, mar, term_in_months)
   if mar == 0
-    amount.to_f / term_in_months
+    amount / term_in_months
   else
-    amount.to_f * (mar / (1 - (1 + mar)**-term_in_months))
+    amount * (mar / (1 - (1 + mar)**-term_in_months))
   end
 end
 
-prompt(MESSAGE[:welcome])
-loop do # main loop
+def valid_amount?(amount)
+  valid_integer(amount)
+end
+
+def valid_rate?(rate)
+  float?(rate)
+end
+
+def valid_term?(term)
+  valid_integer(term) && term.to_i >= 1 && term.to_i <= 30
+end
+
+def retrieve_loan_amount
   loan_amount = ''
   loop do
     prompt(MESSAGE[:amount])
     loan_amount = gets.chomp
-    if number?(loan_amount)
-      break
-    else
-      prompt(MESSAGE[:invalid_number])
-    end
+    break if valid_amount?(loan_amount)
+    prompt(MESSAGE[:invalid_amount])
   end
+  loan_amount.to_i
+end
+
+def retrieve_annual_rate
   annual_rate = ''
   loop do
     prompt(MESSAGE[:rate])
     annual_rate = gets.chomp
-    if number?(annual_rate)
-      break
-    else
-      prompt(MESSAGE[:invalid_number])
-    end
+    break if valid_rate?(annual_rate)
+    prompt(MESSAGE[:invalid_rate])
   end
+  annual_rate.to_f
+end
+
+def retrieve_loan_term
   term_in_years = ''
   loop do
     prompt(MESSAGE[:duration])
     term_in_years = gets.chomp
-    if number?(term_in_years)
-      break
-    else
-      prompt(MESSAGE[:invalid_number])
-    end
+    break if valid_term?(term_in_years)
+    prompt(MESSAGE[:invalid_term])
   end
+  term_in_years.to_i
+end
 
-  monthly_rate = annual_rate.to_f / 100 / 12
-  term_in_months = term_in_years.to_f * 12
+def display_monthly_payment(loan_amount, annual_rate, term_in_years)
+  monthly_rate = annual_rate / 100 / 12
+  term_in_months = term_in_years * 12
   monthly_payment = monthly_payment(loan_amount, monthly_rate, term_in_months)
-  total_payment = (term_in_months.to_f * monthly_payment).round(2)
-  total_interest = (total_payment - loan_amount.to_f).round(2)
+  total_payment = (term_in_months * monthly_payment).round(2)
+  total_interest = (total_payment - loan_amount).round(2)
 
-  if term_in_years == '0'
-    puts "#{loan_amount} immidiately "
-  else
-    puts "Your Loan Amount: $#{loan_amount}"
-    puts "Your annual interest rate (APR): #{annual_rate}%"
-    puts "Monthly Payment: $#{monthly_payment.round(2)}"
-    puts "Time required to Payoff debt: #{term_in_years} years"
-    puts "Total of #{term_in_months} Payments: $#{total_payment}"
-    puts "Total Interest: $#{total_interest.round(2)}"
-  end
+  prompt("Your Loan Amount: $#{loan_amount}")
+  prompt("Your annual interest rate (APR): #{annual_rate}%")
+  prompt("Monthly Payment: $#{monthly_payment.round(2)}")
+  prompt("Time required to Payoff debt: #{term_in_years} years")
+  prompt("Total of #{term_in_months} Payments: $#{total_payment}")
+  prompt("Total Interest: $#{total_interest.round(2)}")
+end
 
+prompt(MESSAGE[:welcome])
+loop do # main loop
+  loan_amount = retrieve_loan_amount
+  annual_rate = retrieve_annual_rate
+  term_in_years = retrieve_loan_term
+  display_monthly_payment(loan_amount, annual_rate, term_in_years)
   puts "_" * 20
-  puts "Would you like to calculate another mortgage? (y or n)"
+  prompt(MESSAGE[:calculate_again])
   answer = gets.chomp.downcase
-  break unless answer.start_with?('y')
+  break unless answer == 'yes' || answer == 'y'
+  system('clear')
 end
 
 prompt(MESSAGE[:thank_you])
